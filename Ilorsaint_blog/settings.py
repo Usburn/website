83978ca11f3664689e3310faa1a3c83cef4f9315
Zaro Ilorsaint <iloz3101@usherbrooke.ca>
2025-05-19 17:09:49 -0400

    decople

--- Ilorsaint_blog/settings.py
+++ Ilorsaint_blog/settings.py
@@ -9,25 +9,36 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 from os import getenv
+
+
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ="django-insecure-(on^uyv(44%3iz9wd@6+viuok79wikb-)ymlwbuir4kq^kbwsp"
+SECRET_KEY =config("THE_SECRET_KEY")
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  
+
+
+
+
+DEBUG = config("MY_DEBUG", cast = bool)
+
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
